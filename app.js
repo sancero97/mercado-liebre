87fb83bcc349f8a9d1d0c3c1a874bc0a38e691ac
Santiago Roman <sancero97@gmail.com>
2022-02-22 20:07:45 -0500

    primer comentario

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(3030, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+app.get('/login2', (req,res)=>{
+    res.sendFile(__dirname + '/views/login2.html');
+});
